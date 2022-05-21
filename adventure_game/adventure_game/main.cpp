f36e6f49f1edf4b864e516472658c8d645d0d152
Jack Keogh <keeeogh@gmail.com>
2022-05-21 15:22:38 +0100

    added_sdl2

--- adventure_game/adventure_game/main.cpp
+++ adventure_game/adventure_game/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <SDL.h>
 
-int main() {
-	std::cout << "Hello World" << std::endl;
+int main(int argc, char* argv[]) {
+	SDL_Window* m_window = SDL_CreateWindow("Adventure Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1080, 720, NULL);
+	SDL_Renderer* m_renderer = SDL_CreateRenderer(m_window, -1, NULL);
+
+	while (true)
+	{
+		SDL_RenderClear(m_renderer);
+
+		SDL_RenderPresent(m_renderer);
+	}
 
 	return 0;
 }
