9e8f4f516297f5fb64739b5ef3025321143310e9
Jack Keogh <keeeogh@gmail.com>
2022-09-19 17:46:44 +0100

    Transition class created

--- adventure_game/adventure_game/Transitions.cpp
+++ adventure_game/adventure_game/Transitions.cpp
@@ -1,1 +1,33 @@
 #include "Transitions.h"
+
+Transitions::Transitions()
+{
+	m_state = TransitionState::null;
+	m_color = { 0,0,0,0 };
+	m_shape = { 0,0,SCREEN_WIDTH,SCREEN_HEIGHT };
+	m_timer = 0.f;
+}
+
+Transitions::~Transitions()
+{
+}
+
+void Transitions::update()
+{
+	switch (m_state)
+	{
+		case TransitionState::fade_in:
+			break;
+		case TransitionState::fade_out:
+			break;
+	}
+}
+
+void Transitions::render(Renderer* r)
+{
+}
+
+void Transitions::reset()
+{
+	m_timer = 0.f;
+}
--- adventure_game/adventure_game/Transitions.h
+++ adventure_game/adventure_game/Transitions.h
@@ -1,5 +1,30 @@
 #pragma once
+#include "RenderingSystem.h"
+
+enum class TransitionState
+{
+	null,
+	fade_in,
+	fade_out
+};
+
 class Transitions
 {
+public:
+	Transitions();
+
+	~Transitions();
+
+	void update();
+
+	void render(Renderer* r);
+
+	void reset();
+
+private:
+	SDL_Color m_color;
+	SDL_Rect m_shape;
+	TransitionState m_state;
+	float m_timer;
 };
 
