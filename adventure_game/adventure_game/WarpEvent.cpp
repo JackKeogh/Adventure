08ea69368a6e9b030411aff89683831366c10e14
Jack Keogh <keeeogh@gmail.com>
2022-09-27 16:40:06 +0100

    Added additional commands

--- adventure_game/adventure_game/WarpEvent.cpp
+++ adventure_game/adventure_game/WarpEvent.cpp
@@ -3,8 +3,10 @@
 WarpEvent::WarpEvent()
 {
 	m_sprite = new Sprite(Renderer::Render(), "assets/Primitives/square.png", { 9,40,27,27 }, { 0, 0,SCREEN_WIDTH,SCREEN_HEIGHT });
-	addCommand(new ChangeOverworldState(OverworldState::Transition_Inside));
+	addCommand(new ChangeOverworldState(OverworldState::Transition_Inside_FadeOut));
 	addCommand(new FadeOut(m_sprite));
+	addCommand(new ChangeOverworldState(OverworldState::Transition_Inside_ChangeWorld));
+	addCommand(new ChangeOverworldState(OverworldState::Transition_Inside_FadeIn));
 	addCommand(new FadeIn(m_sprite));
 	addCommand(new ChangeOverworldState(OverworldState::Inside));
 }
