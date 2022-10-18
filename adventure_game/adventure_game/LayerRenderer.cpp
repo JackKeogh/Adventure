af4d95fee3864593ee338df74fa179db9422e427
Jack Keogh <keeeogh@gmail.com>
2022-10-18 18:40:34 +0100

    Adding layer rendering

---
+++ adventure_game/adventure_game/LayerRenderer.cpp
@@ -0,0 +1,36 @@
+#include "LayerRenderer.h"
+
+std::map<RenderLayer, std::vector<Sprite*>> LayerRenderer::m_layers = std::map<RenderLayer, std::vector<Sprite*>>();
+
+void LayerRenderer::clear()
+{
+	for (int i = 0; i < m_layers.size(); i++)
+	{
+		RenderLayer layer = (RenderLayer)i;
+		m_layers[layer].clear();
+	}
+}
+
+void LayerRenderer::addSprite(RenderLayer l, Sprite* s)
+{
+	m_layers[l].push_back(s);
+}
+
+void LayerRenderer::render(Renderer* r)
+{
+	for (int i = 0; i < m_layers.size(); i++)
+	{
+		RenderLayer layer = (RenderLayer)i;
+		renderLayer(r, layer);
+	}
+
+	clear();
+}
+
+void LayerRenderer::renderLayer(Renderer* r, RenderLayer l)
+{
+	for (int i = 0; i < m_layers[l].size(); i++)
+	{
+		m_layers[l][i]->Render(r);
+	}
+}
---
+++ adventure_game/adventure_game/LayerRenderer.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "stdafx.h"
+#include "RenderingSystem.h"
+#include "Sprite.h"
+
+enum class RenderLayer {
+	Background = 0,
+	Middleground,
+	Foreground,
+	Player
+};
+
+class LayerRenderer
+{
+public:
+
+	static void clear();
+
+	static void addSprite(RenderLayer l, Sprite* s);
+
+	static void render(Renderer* r);
+
+	static void renderLayer(Renderer* r, RenderLayer l);
+
+private:
+	static std::map<RenderLayer, std::vector<Sprite*>> m_layers;
+};
+
