d44aac97a238619c8e0bacb9591ec790b7794274
Jack Keogh <keeeogh@gmail.com>
2022-11-22 15:46:42 +0000

    Updated route 101

--- adventure_game/adventure_game/Route101.cpp
+++ adventure_game/adventure_game/Route101.cpp
@@ -2,9 +2,9 @@
 
 Route101::Route101(Renderer* r)
 {
-	m_foreground = new Sprite(r, "assets/littleroot/littleroot_foreground.png", { 0,0,480,416 }, { 0,-832,960,832 });
-	m_middleGround = new Sprite(r, "assets/littleroot/littleroot_middleground.png", { 0,0,480,416 }, { 0,-832,960,832 });
-	m_background = new Sprite(r, "assets/littleroot/littleroot_BackGround.png", { 0,0,480,416 }, { 0,-832,960,832 });
+	m_foreground = new Sprite(r, "assets/Route101/foreground.png", { 0,0,576,384 }, { 0,-768,1152,768 });
+	m_middleGround = new Sprite(r, "assets/Route101/middleground.png", { 0,0,576,384 }, { 0,-768,1152,768 });
+	m_background = new Sprite(r, "assets/Route101/base.png", { 0,0,576,384 }, { 0,-768,1152,768 });
 	m_name = LocationName::Route101;
 
 	m_nodes = new Route101NodeArea();
